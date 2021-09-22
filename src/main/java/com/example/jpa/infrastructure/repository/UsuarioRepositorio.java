41faff4d9d971f521ac0959b218b092dff9b38c0
jon ander herrero <jonanderherrero@gmail.com>
2021-09-22 13:14:03 +0200

    2 commit

--- src/main/java/com/example/jpa/infrastructure/repository/UsuarioRepositorio.java
+++ src/main/java/com/example/jpa/infrastructure/repository/UsuarioRepositorio.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Integer> {
 
-    @Query("select u from Usuario u where u.user = ?1")
+    @Query("select u from Usuario u where u.name = ?1")
     List<Usuario> getByName(String name);
 
     //Se podia usar directamente esto en lugar del getByName
